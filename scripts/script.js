f58d53e170e0f22212f2afbd6449618de57d5c54
Prashath C <prashathc@Prashaths-MacBook-Air.local>
2019-10-31 01:38:39 -0400

    Wow, really proud of myself, managed to make firebase the source of truth so now the list re-renders everytime firebase gets updated

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,10 @@
-// TODO BONUS:
-// * Sort list from highest to lowest
-// * When count is at 0, pressing the - button removes skill from list
-
 // selecting dom elements for manipulation
 const dbRef = firebase.database().ref();
 const entireForm = document.querySelector('.entireForm');
 const listOfSkills = document.querySelector('.listOfSkills');
 
 // assigning global variables
-let count = 1;
+let count = 1; // change to inital count
 const arrayOfSkills = [];
 
 // function to render newly typed skill and call sendToFirebase function
@@ -16,31 +12,28 @@ function addItemToList(e) {
 	e.preventDefault();
 	const skill = this.querySelector('[name="skill"]').value;
 
-	if (skill == '') {
-		alert('Type something before sending');
+	if (!skill || skill.length > 30) {
+		alert('Type something less than 30 characters but longer than 0');
 		return;
 	}
 
 	arrayOfSkills.push(skill);
 	sendToFirebase(e, skill);
-	dbRef.once('value', updateListFromFirebase);
 	entireForm.reset();
-	return;
 }
 
-// function to render latest list item onto screen
-updateListFromFirebase = (response) => {
+// function to listen to changes in firebase database and render UI on change
+updateListFromFirebase = dbRef.on('value', (response) => {
 	const firebaseArray = Object.values(response.val());
 	const sortedArray = firebaseArray.sort((a, b) =>
 		a.count < b.count ? 1 : -1
 	);
 
-	console.log(sortedArray);
 	listOfSkills.innerHTML = '';
 
 	sortedArray.map((item) => {
 		listOfSkills.innerHTML += `
-	<div>
+	<div class="skillContainer">
 		<li class="skillName">${item.skill}</li>
 		<p class="currentScore">${item.count}</p>
 		<button class="plus">+</button>
@@ -48,7 +41,8 @@ updateListFromFirebase = (response) => {
 	</div>
 	`;
 	});
-};
+	console.log('just updated UI');
+});
 
 // function to send new item to firebase
 sendToFirebase = (event, skill) => {
@@ -59,36 +53,15 @@ sendToFirebase = (event, skill) => {
 	});
 };
 
-// function to load existing list items from firebase on page load
-function updateUIOnPageLoad() {
-	dbRef.once('value', (response) => {
-		const firebaseArray = Object.values(response.val());
-		const sortedArray = firebaseArray.sort((a, b) =>
-			a.count < b.count ? 1 : -1
-		);
-		if (sortedArray.length != 0) {
-			sortedArray.map((item) => {
-				listOfSkills.innerHTML += `
-					<div>
-						<li class="skillName">${item.skill}</li>
-						<p class="currentScore">${item.count}</p>
-						<button class="plus">+</button>
-						<button class="minus">-</button>			
-					</div>`;
-			});
-		}
-	});
-	return;
-}
-
 // function to increase counter by one on button click and update firebase
 function increaseCount(e) {
 	if (!e.target.matches('.plus')) return;
 	const targ = e.target;
 
-	let currentScore = targ.previousElementSibling.innerText;
+	let currentScore = targ.parentElement.querySelector('.currentScore')
+		.innerText;
 	currentScore = Number(currentScore) + 1;
-	targ.previousElementSibling.innerText = currentScore;
+	targ.parentElement.querySelector('.currentScore').innerText = currentScore;
 
 	let skillName = e.target.parentElement.children[0].innerText;
 	const userRef = dbRef.child(`${skillName}`);
@@ -100,7 +73,7 @@ function increaseCount(e) {
 	return;
 }
 
-// function to decrease counter by one on button click and update firebase
+// function to decrease counter by one on button click and delete any skill with a count of 0
 function decreaseCount(e) {
 	if (!e.target.matches('.minus')) return;
 
@@ -108,20 +81,20 @@ function decreaseCount(e) {
 
 	let currentScore = targ.parentElement.children[1].innerText;
 	currentScore = Number(currentScore) - 1;
-	targ.parentElement.children[1].innerText = currentScore;
 	let skillName = e.target.parentElement.children[0].innerText;
 	const userRef = dbRef.child(`${skillName}`);
-
 	const count = currentScore;
-	userRef.update({
-		count: count
-	});
 
-	return;
+	if (count == 0) {
+		userRef.remove();
+	} else {
+		userRef.update({
+			count: count
+		});
+	}
 }
 
-// event listeners
-window.addEventListener('load', updateUIOnPageLoad);
+window.addEventListener('load', updateListFromFirebase);
 entireForm.addEventListener('submit', addItemToList);
 listOfSkills.addEventListener('click', increaseCount);
 listOfSkills.addEventListener('click', decreaseCount);
