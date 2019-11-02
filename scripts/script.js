f81edc46fce94992fb68b0f22f9d9a121a6af520
Prashath C <prashathc@Prashaths-MacBook-Air.local>
2019-11-02 10:44:08 -0400

    Fixed bug with scripts rendering on screen

--- scripts/script.js
+++ scripts/script.js
@@ -130,33 +130,26 @@ const listOfSkills = document.querySelector('.listOfSkills');
 const textAreaUserInput = document.querySelector('#skill');
 
 // assigning global variables
-let count = 1; // change to inital count
 const arrayOfSkills = [];
+let arrayOfMatchedWords = [];
 let arrayOfHighlightedWords = [];
 
 // function to render newly typed skill and call sendToFirebase function
 function addItemToList(e) {
+	let count = 1; // change to inital count
+
 	e.preventDefault();
 	arrayOfHighlightedWords.map((highlightedWord) => {
-		if (!highlightedWord) {
-			alert('Type something');
-			entireForm.reset();
-			this.querySelector('#skill').focus();
-			return;
-		}
-
-		dbRef.on('value', function(snapshot) {
-			console.log(snapshot.val()[`${highlightedWord}`]);
-		});
+		console.log(highlightedWord);
 
 		arrayOfSkills.push(highlightedWord);
-		sendToFirebase(e, highlightedWord);
+		sendToFirebase(e, highlightedWord, count);
 		entireForm.reset();
 	});
 }
 
 // function to send new item to firebase
-sendToFirebase = (event, skill) => {
+sendToFirebase = (event, skill, count) => {
 	event.preventDefault();
 	const newSkill = { skill, count };
 	dbRef.update({
@@ -185,28 +178,42 @@ updateListFromFirebase = dbRef.on('value', (response) => {
 	});
 });
 
-// function that takes userinput and compares it to keyword list and returns the matches
-function findMatches(wordToMatch, hugeListOfKeywords) {
-	let currentWordFromUserInput = wordToMatch.split(' ');
-	let arrayOfMatchedWords = [];
-	hugeListOfKeywords.filter((word) => {
-		currentWordFromUserInput.map((item) => {
-			if (word.includes(item) && item != '') {
-				arrayOfMatchedWords.push(word);
-			}
-		});
-	});
-	return arrayOfMatchedWords;
-}
-
-// function that creates array of highlighted words in the text area
+// create array of highlighted words from text area
 createArrayOfHightlightedWords = ({ target }) => {
 	const userInput = target.value;
-	let matchingWords = findMatches(userInput, hugeListOfKeywords);
+	let matchingWords = findMatches(userInput);
 	arrayOfHighlightedWords = [...matchingWords];
 };
 
-// function to increase/decrease counter by one on button click and delete any skill with a count of 0
+// returns matches
+function findMatches(userInput) {
+	let finalString = removePunctation(userInput);
+	let arrayOfWordsFromUserInput = finalString.split(' ');
+
+	compareUserInput(hugeListOfKeywords, arrayOfWordsFromUserInput);
+
+	return arrayOfMatchedWords;
+}
+
+// compares hugeListOfKeywords with userInput
+
+function compareUserInput(hugeListOfKeywords, arrayOfWordsFromUserInput) {
+	hugeListOfKeywords.filter((keyword) => {
+		arrayOfWordsFromUserInput.map((currentWord) => {
+			if (keyword === currentWord && currentWord != '') {
+				arrayOfMatchedWords.push(keyword);
+			}
+		});
+	});
+}
+
+// remove punctation and extra spaces from userInput
+function removePunctation(userInput) {
+	let punctuationless = userInput.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '');
+	return punctuationless.replace(/\s{2,}/g, ' ');
+}
+
+// update counter & delete any skill with a count of 0
 function updateCount({ target }) {
 	let count = target.parentElement.querySelector('.count').innerText;
 	let skillName = target.parentElement.querySelector('.skillName').innerText;
